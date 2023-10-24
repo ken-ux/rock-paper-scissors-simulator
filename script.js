2025b7195ad531758a7cdcd102579c9da3b6b4ed
Kenny Nguyen <sokenns@gmail.com>
2023-10-24 15:16:40 -0700

    Add event listeners to buttons to play game

--- script.js
+++ script.js
@@ -1,58 +1,48 @@
 // Generate a random choice from the computer
 function getComputerChoice() {
-  let choices = ["Rock", "Paper", "Scissors"];
+  let choices = ["rock", "paper", "scissors"];
   let choice = Math.floor(Math.random() * 3);
   console.log("The computer chose " + choices[choice] + ".");
   return choices[choice];
 }
 
-// Prompt player for a choice. The input is case-insensitive
-function getPlayerChoice() {
-  let choice = prompt("Rock, paper, or scissors?");
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button) => {
+  button.addEventListener("click", () => playRound(button.id));
+});
 
-  // If prompt is cancelled without user input, end the prompt session
-  if (choice === null) {
-    console.log("You cancelled the prompt. Please make a choice.");
-    return null;
-  }
-
-  // Trim white space from any user input
-  choice = choice.trim();
-
-  if (choice === "") { // Check if string is empty
-    console.log("Your choice is empty. Please make a choice.");
-    return null;
-  } else { // If string is not empty, capitalize it
-    choice = choice[0].toUpperCase() + choice.slice(1).toLowerCase();
-    console.log("You chose " + choice + ".");
-    return choice;
-  }
-}
+const result = document.querySelector("#result");
 
 // Evaluates whether the player or the computer won with their choice
-function playRound(playerSelection, computerSelection) {
+function playRound(playerSelection) {
+  let computerSelection = getComputerChoice();
+  roundResult = evaluateRound(playerSelection, computerSelection);
+  result.textContent = roundResult;
+}
+
+function evaluateRound(playerSelection, computerSelection) {
   // Return a tie and end game
   if (playerSelection === computerSelection) {
     return "It's a tie.";
   }
 
   // Play game normally
-  if (playerSelection === "Rock") {
-    if (computerSelection === "Paper") {
+  if (playerSelection === "rock") {
+    if (computerSelection === "paper") {
       return "You lose! Paper beats rock.";
     } else {
       // computerSelection is Scissors
       return "You win! Rock beats scissors.";
     }
-  } else if (playerSelection === "Paper") {
-    if (computerSelection === "Rock") {
+  } else if (playerSelection === "paper") {
+    if (computerSelection === "rock") {
       return "You win! Paper beats rock.";
     } else {
       // computerSelection is Scissors
       return "You lose! Scissors beats paper.";
     }
-  } else if (playerSelection === "Scissors") {
-    if (computerSelection === "Rock") {
+  } else if (playerSelection === "scissors") {
+    if (computerSelection === "rock") {
       return "You lose! Rock beats scissors.";
     } else {
       // computerSelection is Paper
@@ -64,10 +54,10 @@ function playRound(playerSelection, computerSelection) {
 }
 
 // Plays a game of rock, paper, scissors
-function playGame() {
-  let playerSelection = getPlayerChoice();
-  let computerSelection = getComputerChoice();
-  console.log(playRound(playerSelection, computerSelection));
-}
+// function playGame() {
+//   let playerSelection = getPlayerChoice();
+//   let computerSelection = getComputerChoice();
+//   console.log(playRound(playerSelection, computerSelection));
+// }
 
-playGame();
+// playGame();
